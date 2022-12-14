c7b60bf31a46ac7a2b90955686e2740ca7a08f02
jan <j_sten07@uni-muenster.de>
2022-12-14 18:00:33 +0100

    belongs to changed structure

---
+++ app/routes/footage.routes.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+    const footage = require("../controllers/footage.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Footage
+    router.post("/", footage.create);
+  
+    // Retrieve all Footages
+    router.get("/", footage.findAll);
+  
+    // Retrieve a single Footage with id
+    router.get("/:id", footage.findOne);
+  
+    // Update a Tutorial with id
+    router.put("/:id", footage.update);
+  
+    // Delete a Tutorial with id
+    router.delete("/:id", footage.delete);
+  
+    app.use("/api/footage", router);
+};

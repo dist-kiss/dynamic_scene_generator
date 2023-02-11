ed12a27f66722c2f7f2b8babe2565c157f5c29a9
Simon Schröder <simon.p.s@t-online.de>
2023-02-11 16:24:28 +0100

    added Keywords for Signs and Crowds

--- generator.js
+++ generator.js
@@ -18,30 +18,35 @@ const { Overlay_belongs_to_scenario } = require('./Connect_to_IVE_API/Relationsh
 // This is the data that will come from the model
 async function main() {
 let data = {
-    "Scenario_name" : "SCENARIO2",
+    "Scenario_name" : "SCENARIO1",
     "Location_name" : "LOCATION4",
-    "degree" : 3,
+    "degree" : 2,
     "Sign_1" : "Stop",
-    "Sign_2" : "Stop",
-    "Sign_3" : "Stop"
+    "Sign_2" : "Go",
+    //"Sign_3" : "Stop",
+    "Crowd_1" : "2",
+    "Crowd_2" : "1",
+    //"Crowd_3" : "2"
 }
 
-
-
+let Keywords = [
+    "Sign",
+    "Crowd"
+]
 
 
 //Does the scenario already exist? If not create scenario with the given Name 
 let ScenarioArray = await Get_scenarios();
 let Currentscenario = await ScenarioArray.find(scenario => scenario.name === data.Scenario_name);
 
-    if (Currentscenario) {
-        console.log(`Scenario with name ${data.Scenario_name} found:`, Currentscenario);
-    } else {
-        console.log(`Scenario with name ${data.Scenario_name} not found.`);
-        let scenariodata =JSON.stringify({"name" : data.Scenario_name,"description" : ""});
-        Currentscenario = await Create_scenario(scenariodata);
-        console.log(`Scenario with name ${data.Scenario_name} created.`, Currentscenario);
-  }
+if (Currentscenario) {
+    console.log(`Scenario with name ${data.Scenario_name} found:`, Currentscenario);
+} else {
+    console.log(`Scenario with name ${data.Scenario_name} not found.`);
+    let scenariodata =JSON.stringify({"name" : data.Scenario_name,"description" : ""});
+    Currentscenario = await Create_scenario(scenariodata);
+    console.log(`Scenario with name ${data.Scenario_name} created.`, Currentscenario);
+}
     
 
 //Does the location already exist at the given scenario? If not create location with the given Name and relate it to the scenario
@@ -49,32 +54,26 @@ let Currentscenario = await ScenarioArray.find(scenario => scenario.name === dat
 let LocationArray = await Get_locations_by_scenario(Currentscenario.scenario_id);
 let Currentlocation = LocationArray.find(location => location.name === data.Location_name);
 
-    if(Currentlocation){
-        console.log(`Location with name ${data.Location_name} found`, Currentlocation);
-    } else {
-        console.log(`Location with name ${data.Location_name} not found in Scenario ${data.Scenario_name}.`);
-        let locationdata = JSON.stringify({"name" : data.Location_name, "description" : "", "lat": "51.96712176326085", "lng" : "7.601288886457439" , "location_type" : ""});
-        Currentlocation = await Create_location(locationdata);
-        await Location_belongs_to_scenario(JSON.stringify({
-            "scenario_id": Currentscenario.scenario_id, 
-            "location_id": Currentlocation.location_id
-          }));
-        console.log(`Location with name ${data.Location_name} created and related to ${data.Scenario_name}`, Currentlocation);
-    }
+if(Currentlocation){
+    console.log(`Location with name ${data.Location_name} found`, Currentlocation);
+} else {
+    console.log(`Location with name ${data.Location_name} not found in Scenario ${data.Scenario_name}.`);
+    let locationdata = JSON.stringify({"name" : data.Location_name, "description" : "", "lat": "51.96712176326085", "lng" : "7.601288886457439" , "location_type" : ""});
+    Currentlocation = await Create_location(locationdata);
+    await Location_belongs_to_scenario(JSON.stringify({
+        "scenario_id": Currentscenario.scenario_id, 
+        "location_id": Currentlocation.location_id
+        }));
+    console.log(`Location with name ${data.Location_name} created and related to ${data.Scenario_name}`, Currentlocation);
+}
 
 
-
-
-
-
-
-//Choose video depending on degree of the intersection (anything else?)
+//Choose video depending on degree of the intersection (anything else?) TODO: Add random selection
 if(data.degree == 2){
     //choose random video with 2 exits
-     //TODO: implement Videos with 2 exits
+     videoId = 86;
 }else if(data.degree == 3){
     //choose random video with 3 exits and get its (IVE) Id 
-    //TODO: implement random video selection
     videoId = 4;
 }  
 //assign the video_id of the chosen video to the Originvideo variable, create new instance with same URL called Currentvideo and relate it to scenario and location
@@ -101,60 +100,68 @@ let vidscenario = await Video_belongs_to_scenario(JSON.stringify({
     "video_id": Currentvideo.video_id
     }));
     
-console.log(`Video created put into ${data.Location_name} Location and Scenario ${data.Scenario_name}.`, Currentvideo )
+console.log(`Video created and put into ${data.Location_name} Location and Scenario ${data.Scenario_name}.`, Currentvideo )
      
 
-//Now choose Sign overlays for every possible path
-
+//Now choose overlays for every possible path
+let Anchorpoint, AnchorpointID, Anchorpoint_looper, AnchorpointData, embedding_details, exit;
 for(let i =1; i <= data.degree; i++){
-    let looper = `Sign_${i}`;
-    if(data[looper] == "Stop"){
-        //Hardcoded ID of the One way sign here
-        PedOneWayID = 87;
+    
 
+    //get info about Exit from videodata.js for now 
+    exit = `Exit ${i}`;
+    let exitData = Originvideo.exits.find(exitData => exitData.name === exit);
+    if (!exitData) {
+    console.error(`Exit ${exit} not found in video with id ${videoId}.`);
+    return;
+    }
 
+    
+    for(let j = 0; j < Keywords.length; j++){
+        Anchorpoint = Keywords[j];
+        Anchorpoint_looper = `${Anchorpoint}_${i}`;
         
-        let exit = `Exit ${i}`;
-        let Anchorpoint = "Sign";
-        //get info about Anchorpoints from videodata.js for now 
-        let exitData = Originvideo.exits.find(exitData => exitData.name === exit);
-        if (!exitData) {
-        console.error(`Exit ${exit} not found in video with id ${videoId}.`);
-        return;
+        //Ids of overlays TODO: Maybe incorporate this into the videodata?
+        if(data[Anchorpoint_looper] == "Stop"){
+            AnchorpointID = 87;
+        }else if( data[Anchorpoint_looper] == "Go"){
+            AnchorpointID = 88;
+        }else if( data[Anchorpoint_looper] == 1){
+            AnchorpointID = 93;
+        }else if( data[Anchorpoint_looper] == 2){
+            AnchorpointID = 95;
+        }else{
+            continue;
         }
-        const AnchorpointData = exitData.Anchorpoints.find(AnchorpointData => AnchorpointData.name === Anchorpoint);
+        
+        //get info about Anchorpoint from videodata.js for now 
+        AnchorpointData = exitData.Anchorpoints.find(AnchorpointData => AnchorpointData.name === Anchorpoint);
         if (!AnchorpointData) {
         console.error(`Anchorpoint ${Anchorpoint} not found in exit ${exit} of video with id ${videoId}.`);
         return;
         }
         
-        var embedding_details = JSON.stringify({
-            "overlay_id": PedOneWayID,
+        embedding_details = JSON.stringify({
+            "overlay_id": AnchorpointID,
             "video_id": Currentvideo.video_id,
             "x": AnchorpointData.x,
             "y": AnchorpointData.y,
             "z": AnchorpointData.z,
-            "description": "",
+            "description": "test",
             "rz": AnchorpointData.rz,
             "rx": AnchorpointData.rx,
             "ry": AnchorpointData.ry,
             "h": AnchorpointData.h,
             "w": AnchorpointData.w,
             "display": "true",
-            "d": "0"
-          });
-          let temp = await Embedded_in(embedding_details);
-          let overlayscenario = await Overlay_belongs_to_scenario(JSON.stringify({
+            "d": "1"
+        });
+        await Embedded_in(embedding_details);
+        await Overlay_belongs_to_scenario(JSON.stringify({
             "scenario_id" : Currentscenario.scenario_id,
-            "overlay_id": PedOneWayID
+            "overlay_id": AnchorpointID
             }));
-          console.log(`Created Stopsign at ${looper}`);
-
-
-
-    }else if( data[looper] == "Go"){
-        console.log(`${looper} will be a Gosign`);
-
+        console.log(`Created ${data[Anchorpoint_looper]} ${Anchorpoint} at Exit ${i}`);
     }
 }
 
--- videodata.js
+++ videodata.js
@@ -1,12 +1,14 @@
 const videos = [
     {
-      id: 1,
+      id: 86,
+      name: "QB 29 dir 11 28",
+      URL: "/Locations/QB_29_dir_11_28",
       exits: [
         {
           name: "Exit 1",
           Anchorpoints: [
-            { name: "Anchorpoint A", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
-            { name: "Anchorpoint b", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
+            { name: "Sign", x: -18.6487, y: 1.1147, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+            { name: "Crowd", x: -9.7, y: -1.3, z: 1.15, rx: 0, ry: 0, rz: 0, w: 2.7, h: 2.9 },
             { name: "Anchorpoint c", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
             { name: "Anchorpoint d", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
           ]
@@ -14,8 +16,8 @@ const videos = [
         {
           name: "Exit 2",
           Anchorpoints: [
-            { name: "Anchorpoint A", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
-            { name: "Anchorpoint b", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
+            { name: "Sign", x: 17.9157, y: 2.4451, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+            { name: "Crowd", x: 12.402, y: -0.6009, z: 0.3653, rx: 0, ry: 0, rz: 0, w: 2.7, h: 2.8 },
             { name: "Anchorpoint c", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
             { name: "Anchorpoint d", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
           ]
@@ -31,7 +33,7 @@ const videos = [
           name: "Exit 1",
           Anchorpoints: [
             { name: "Sign", x: -16.2672, y: 1.4742, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
-            { name: "Anchorpoint b", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
+            { name: "Crowd", x: -26.3061, y: -1.0025, z: -4.4726, rx: 0, ry: 0, rz: 0, w: 6.7, h: 5.58 },
             { name: "Anchorpoint c", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
             { name: "Anchorpoint d", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
           ]
@@ -40,7 +42,7 @@ const videos = [
           name: "Exit 2",
           Anchorpoints: [
             { name: "Sign", x: 5.208, y: 1.0307, z: -1.9498, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
-            { name: "Anchorpoint A", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
+            { name: "Crowd", x: 1.2, y: -0.2, z: -2.3, rx: 0, ry: 0, rz: 0, w: 3, h: 3 },
             { name: "Anchorpoint A", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
             { name: "Anchorpoint A", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
           ]
@@ -49,7 +51,7 @@ const videos = [
           name: "Exit 3",
           Anchorpoints: [
             { name: "Sign", x: 18.3742, y: 0.5753, z: 0, rx: 0, ry: -0.0013997540600994521, rz: 0.008300785922485033, w: 3, h: 2 },
-            { name: "Anchorpoint b", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
+            { name: "Crowd", x: 24.9897, y: -2.0243, z: -4.4726, rx: 0, ry: 0, rz: 0, w: 6.7, h: 5.58 },
             { name: "Anchorpoint c", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
             { name: "Anchorpoint d", x: 1, y: 2, z: 3, rx: 4, ry: 5, rz: 6, w: 7, h: 8 },
           ]

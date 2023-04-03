26cd08bef4e0332562df25415b0b5fd8116b5123
jan <j_sten07@uni-muenster.de>
2023-04-03 17:06:12 +0200

    add examples with small mistakes

--- examples/post_footage.js
+++ examples/post_footage.js
@@ -8,16 +8,56 @@ const { Create_video } = require("../Connect_to_IVE_API/Videos/Create_video");
 const { scenario_details } = require("../datastructures");
 const { video_details } = require("../datastructures");
 const { location_details } = require("../datastructures");
-const {schema_video} = require('./sampledata');
+const { schema_video } = require('./sampledata');
+
+const wrong_footage = {
+    id: 71,
+    name: "QB_08_dir_07_09",
+    // video: "/quakenbrueck/QB_8_dir_7_9",
+    degree: 4,
+    distance_overlays: [
+        {
+            direction: 1,
+            screen_coordinates:
+                { x: -12.2455, y: -2.5548, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+        },
+        {
+            direction: 2,
+            screen_coordinates:
+                { x: 10.4542, y: -1.7619, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+        }
+    ],
+    sign_overlays: [
+        {
+            direction: 1,
+            screen_coordinates:
+                { x: -12.6377, y: 1.7075, z: 1.1536, rx: -0.0016, ry: -0.092, rz: -0.017, w: 2.4399, h: 2 },
+            // type: 'forbidden'
+        },
+        {
+            direction: 2,
+            screen_coordinates:
+                { x: 9.0164, y: 2.3852, z: -3.7665, rx: 0, ry: 0.3941, rz: 0, w: 3, h: 2 },
+            // type: 'ows_right' 
+        }
+    ],
+    crowd_overlays: [
+        {
+            direction: 1,
+            screen_coordinates:
+                { x: -12.2455, y: -1.5548, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+        },
+        {
+            direction: 2,
+            screen_coordinates:
+                { x: 10.4542, y: -0.7619, z: 0, rx: 0, ry: 0, rz: 0, w: 3, h: 2 },
+        }
+    ],
+}
 
 async function doPostRequest() {
 
     let url = 'http://localhost:5000/api/footage';
-    // let data = {
-    //     video: "http://...",
-    //     name: "valid_test2",
-    //     degree: 4
-    //  };
 
     let res = await fetch(url, {
         method: 'POST',
@@ -32,13 +72,10 @@ async function doPostRequest() {
         let text = await res.text();
         return text;
 
-        // let ret = await res.json();
-        // console.log(ret);
-        // return JSON.parse(ret.data);
-
     } else {
-        console.log(res);
-        return `HTTP error: ${res.status}`;
+        let text = await res.json();
+        console.log(text);
+        return `HTTP error: ${res.status}; ${text.message}`;
     }
 }
 

de36f6623b3dccf2303ffb66897394d79c75eddd
Simon Schröder <simon.p.s@uni-muenster.de>
2023-06-22 16:52:37 +0200

    configure IVE environment

--- app/config/config.js
+++ app/config/config.js
@@ -5,5 +5,8 @@ const {
 } = process.env;
 
 module.exports = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+  IVE_URL: process.env.IVE_URL,
+  IVE_USER: process.env.IVE_USER,
+  IVE_PW: process.env.IVE_PW
 };

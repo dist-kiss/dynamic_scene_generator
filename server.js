d5d3e9cdadf20a1cbb3c614aa0204d92056d5bcd
Simon Schröder <simon.p.s@t-online.de>
2023-06-28 14:29:26 +0200

    move javascript code from index.html and server.js to index.js

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended:true
+  extended: true
 }))
 
 const db = require("./app/models");
-const Scene = db.scenes;
 
 db.mongoose
   .connect(db.url, {
@@ -26,57 +25,11 @@ db.mongoose
   .catch(err => {
     console.log("Cannot connect to the database!", err);
     process.exit();
-});
+  });
 
-
-//Not sure where this should go maybe just stay here?
-app.post("/createscene",(req,res)=>{
-signs = new Array();
-  for (let i = 0; i < req.body.sign.length; i++) {
-    signs.push({
-      "direction" : req.body.signdirection[i], "sign": req.body.sign[i]
-    })
-  } 
-crowds = new Array();
-for (let i = 0; i < req.body.crowd.length; i++) {
-  crowds.push({
-    "direction" : req.body.crowddirection[i], "crowdedness": req.body.crowd[i]
-  })
-}
-distances = new Array();
-for (let i = 0; i < req.body.distance.length; i++) {
-  distances.push({
-    "direction" : req.body.distancedirection[i], "distance": req.body.distance[i]
-  })
-}
-const scene = new Scene({
-  scenario_name: req.body.scenario_name,
-  location_name: req.body.location_name,
-  degree: req.body.degree,
-  signs: signs,
-  crowds: crowds,
-  distances: distances
-})
-const { generate } = require('./generator');
-generate(scene);
-
-
-
-  scene.save((err) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send('Error saving scene to Database');
-    } else {
-      console.log('Scene saved to database');
-      return res.redirect('creation_success.html')
-    }
-  })
-})
-
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.set({
-      "Allow-access-Allow-Origin": '*'
+    "Allow-access-Allow-Origin": '*'
   })
   return res.redirect('index.html');
 });
